2400d957ac69d9068ccf6a5cb1a3d5d0ca4205da
Danila <igusevdanila@gmail.com>
2023-12-10 13:24:02 +0400

    task4 update

--- src/main/gusev/java23/j1_05/task4/FindAndReplace.java
+++ src/main/gusev/java23/j1_05/task4/FindAndReplace.java
@@ -16,54 +16,53 @@ public class FindAndReplace {
     }
 
     public Integer[] find(String forSearch) {
-        List<Integer> list = new ArrayList<Integer>();
         forSearch = forSearch.toLowerCase();
         String infoToLower = this.info.toString().toLowerCase();
-        int overlap = infoToLower.indexOf(forSearch);
-        while (overlap != -1) {
-            list.add(overlap);
-            overlap = infoToLower.indexOf(forSearch, overlap + 1);
-        }
-        Integer[] answer = list.toArray(new Integer[0]);
-        return answer;
-    }
-
-    public int replace(String forSearch, String forReplace) {
-        Integer[] getPositions = find(forSearch);
-        int answer = getPositions.length;
-        for (int i = 0; i < getPositions.length; i++) {
-            this.info.replace(getPositions[i], getPositions[i] + forSearch.length(), forReplace);
-        }
+        Integer[] answer = commonFindCode(forSearch, infoToLower);
         return answer;
     }
 
     public Integer[] find(String forSearch, boolean withCase) {
         if (withCase) {
-            List<Integer> list = new ArrayList<Integer>();
-            int overlap = this.info.indexOf(forSearch);
-            while (overlap != -1) {
-                list.add(overlap);
-                overlap = this.info.indexOf(forSearch, overlap + 1);
-            }
-            Integer[] answer = list.toArray(new Integer[0]);
+            String whereSearch = new String(this.info);
+            Integer[] answer = commonFindCode(forSearch, whereSearch);
             return answer;
         } else {
             Integer[] answer = find(forSearch);
             return answer;
         }
     }
+    public Integer[] commonFindCode(String forSearch, String whereSearch){
+        List<Integer> list = new ArrayList<Integer>();
+        int overlap = whereSearch.indexOf(forSearch);
+        while (overlap != -1) {
+            list.add(overlap);
+            overlap = whereSearch.indexOf(forSearch, overlap + 1);
+        }
+        Integer[] answer = list.toArray(new Integer[0]);
+        return answer;
+    }
+    public int replace(String forSearch, String forReplace) {
+        Integer[] getPositions = find(forSearch);
+        int answer = commonReplaceCode(getPositions, forSearch, forReplace);
+        return answer;
+    }
 
     public int replace(String forSearch, String forReplace, boolean withCase) {
         if (withCase) {
             Integer[] getPositions = find(forSearch, withCase);
-            int answer = getPositions.length;
-            for (int i = 0; i < getPositions.length; i++) {
-                this.info.replace(getPositions[i], getPositions[i] + forSearch.length(), forReplace);
-            }
+            int answer = commonReplaceCode(getPositions, forSearch, forReplace);
             return answer;
         } else {
             int answer = replace(forSearch, forReplace);
             return answer;
         }
+    }
+    public int commonReplaceCode(Integer[] getPositions, String forSearch, String forReplace){
+        int answer = getPositions.length;
+        for (int i = 0; i < getPositions.length; i++) {
+            this.info.replace(getPositions[i], getPositions[i] + forSearch.length(), forReplace);
+        }
+        return answer;
     }
 }
