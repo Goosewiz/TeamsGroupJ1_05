7e96ffe879910d68deb5f517da29b41e7030ea09
Danila <igusevdanila@gmail.com>
2023-12-10 15:58:25 +0400

    task3 update

--- src/main/gusev/java23/j1_05/task3/Transliteration.java
+++ src/main/gusev/java23/j1_05/task3/Transliteration.java
@@ -1,10 +1,16 @@
 package gusev.java23.j1_05.task3;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Transliteration {
-    private static String[] russianLetters = new String[]{"А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н",
-            "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я"};
-    private static String[] englishLetters = new String[]{"A", "B", "V", "G", "D", "E", "Yo", "Zh", "Z", "I", "J", "K", "L", "M", "N", "O", "P", "R",
-            "S", "T", "U", "F", "H", "C", "Ch", "Sh", "Shch", "\"", "Y", "'", "E", "Yu", "Ya"};
+    private final static String[] russianLetters = new String[]{"А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М",
+            "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я"};
+    private final static String[] englishLetters = new String[]{"A", "B", "V", "G", "D", "E", "Yo", "Zh", "Z", "I", "J", "K", "L", "M",
+            "N", "O", "P", "R", "S", "T", "U", "F", "H", "C", "Ch", "Sh", "Shch", "\"", "Y", "'", "E", "Yu", "Ya"};
+    private final static List<String> listOfEnglishLetters = Arrays.asList("A", "B", "V", "G", "D", "E", "Yo", "Zh", "Z", "I", "J", "K", "L", "M",
+            "N", "O", "P", "R", "S", "T", "U", "F", "H", "C", "Ch", "Sh", "Shch", "\"", "Y", "'", "E", "Yu", "Ya", "yo", "zh", "ch",
+            "sh", "shch", "yu", "ya");
 
     public static String russianToEnglish(String russian) {
         StringBuilder answer = new StringBuilder();
@@ -30,37 +36,16 @@ public class Transliteration {
         StringBuilder answer = new StringBuilder();
         StringBuilder in = new StringBuilder(english);
         while (in.length() != 0) {
-            String temp = in.substring(0, 1);
-            in.delete(0, 1);
-            if (temp.equals("Y") || temp.equals("y")) {
-                String next = in.substring(0, 1);
-                if (next.equals("o") || next.equals("u") || next.equals("a")) {
-                    temp = temp + next;
-                    in.delete(0, 1);
-                }
-            } else if (temp.equals("Z") || temp.equals("z")) {
-                String next = in.substring(0, 1);
-                if (next.equals("h")) {
-                    temp = temp + next;
-                    in.delete(0, 1);
-                }
-            } else if (temp.equals("C") || temp.equals("c")) {
-                String next = in.substring(0, 1);
-                if (next.equals("h")) {
-                    temp = temp + next;
-                    in.delete(0, 1);
-                }
-            } else if (temp.equals("S") || temp.equals("s")) {
-                String next = in.substring(0, 1);
-                if (next.equals("h")) {
-                    temp = temp + next;
-                    in.delete(0, 1);
-                    String nextTwo = in.substring(0,2);
-                    if (nextTwo.equals("ch")){
-                        temp = temp + nextTwo;
-                        in.delete(0, 2);
-                    }
-                }
+            String temp;
+            if (in.length() >= 4 && listOfEnglishLetters.contains(in.substring(0, 4))) {
+                temp = in.substring(0, 4);
+                in.delete(0, 4);
+            } else if (in.length() >= 2 && listOfEnglishLetters.contains(in.substring(0, 2))) {
+                temp = in.substring(0, 2);
+                in.delete(0, 2);
+            } else {
+                temp = in.substring(0, 1);
+                in.delete(0, 1);
             }
             int j = 0;
             while ((j < 33) && ((!englishLetters[j].toLowerCase().equals(temp)) && (!englishLetters[j].equals(temp)))) {
